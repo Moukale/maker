13595b3653922dc49cd7b13d956af40d445fca67
Michael <michael.ymele@gmail.com>
2017-02-09 00:01:51 +0100

    Add default port (3000) for server

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get('/', function(req, res){
 });
 
 //////
-app.listen(process.env.PORT);
-console.log("Server running on port "+process.env.PORT);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Server running on port "+port);
 
 
